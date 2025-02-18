f1cf08be39563dbc58f06d6cf1797df9a1d1fe46
muhammadaly0175 <muhammadaly0175@gmail.com>
2025-02-18 20:44:58 +0200

    100th commit

--- src/components/ResetPassword/ResetPassword.jsx
+++ src/components/ResetPassword/ResetPassword.jsx
@@ -55,7 +55,7 @@ const ResetPassword = () => {
             <section>
                 <div className='container py-10 mx-auto'>
                     <form onSubmit={formik.handleSubmit} className='w-[60%] mx-auto'>
-                        <h1 className='text-3xl font-bold text-green-700 pb-10'>Enter your new info</h1>
+                        <h1 className='text-3xl font-bold text-green-700 pb-10'>Enter your new info:</h1>
                         <div className="relative z-0 w-full mb-5 group">
                             <input onBlur={formik.handleBlur} onChange={formik.handleChange} value={formik.values.email} type="email" name="email" id="email" className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none focus:outline-none focus:ring-0 focus:border-green-600 peer" placeholder=" " />
                             <label htmlFor="email" className="peer-focus:font-medium absolute text-sm text-gray-500 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-green-600 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email</label>
