639c7c94d72546d7b8f97bd6137f71303678aaf9
muhammadaly0175 <muhammadaly0175@gmail.com>
2025-02-18 20:13:06 +0200

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -31,6 +31,7 @@ function App() {
   const router = createBrowserRouter([
     {
       path: "", element: <Layout />, children: [
+        { path: "/", element: <Login />, },
         { path: "/login", element: <Login />, },
         { path: "/register", element: <Register />, },
         { path: "/forgotpassword", element: <ForgotPassword />, },
